3cf34b32d2131707d56df7d469234fb9b6808a03
teacher Li <1227380089@qq.com>
2021-01-11 19:21:43 +0800

    Create python输出日志.py

---
+++ python输出日志.py
@@ -0,0 +1,7 @@
+import logging
+logging.debug("this is a bug information")
+
+logging.info('this is a info information')
+logging.warning('this is a warning information')
+logging.error('this is a error information')
+logging.critical('this is a critical information')
